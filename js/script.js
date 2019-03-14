299521952e7576f767ee76bc8f00825a712299aa
valery <valerycorral@ccpaedu.com>
2019-03-14 00:53:20 +0000

    made image on screan

--- js/script.js
+++ js/script.js
@@ -4,8 +4,21 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+
+    $('.gallery').empty();
+    var searchTerm = $('#search-term').val();
+    var url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC";
+    
+    $.ajax({
+        url: url,
+        method: "GET",
+        success: function(response){
+            var gif =response.data[0] 
+            // gif.embed_url
+            // console.log(gif);
+      $('body').append("<img src="+ gif.images.original.url + ">");
+        },
+    }); 
 });
+
 
